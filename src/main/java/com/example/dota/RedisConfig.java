8d18e6800aa498872f9ae8cd0193ee10acc93302
rubick.xi <rubick.xi@d1m.cn>
2018-11-29 17:17:49 +0800

    first commit

---
+++ src/main/java/com/example/dota/RedisConfig.java
@@ -0,0 +1,7 @@
+package com.example.dota;
+
+/**
+ * Created by D1M on 2018/11/29.
+ */
+public class RedisConfig {
+}
---
+++ src/main/java/com/example/dota/util/redis/RedisUtil.java
@@ -0,0 +1,7 @@
+package com.example.dota.util.redis;
+
+/**
+ * Created by D1M on 2018/11/29.
+ */
+public class RedisUtil {
+}
